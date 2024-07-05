1c8bacc72f5eabf74a64481fc2a58c7ded264ff3
Renzo Carianga <renzocarianga@Lesters-MacBook-Pro.local>
2024-07-05 17:29:44 +0800

    add role to user seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -7,6 +7,8 @@ use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
+use Spatie\Permission\Models\Role;
+
 class UserSeeder extends Seeder
 {
     /**
@@ -15,5 +17,11 @@ class UserSeeder extends Seeder
     public function run(): void
     {
        $user= User::updateOrCreate(['email' => 'renzo.carianga@gmail.com'], ['name' => 'Renzo Carianga','password'=>Hash::make('weneverknow')]);
+        $superAdminRole = Role::where('name', 'super_admin')->first();
+
+        if ($superAdminRole) {
+            // Assign super_admin role to user with id 1
+            $user->roles()->syncWithoutDetaching([$superAdminRole->id]);
+        }
     }
 }
