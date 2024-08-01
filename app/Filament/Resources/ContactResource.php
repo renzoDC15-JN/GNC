15aa541bbf2ba8b85cb7b5274575651003df14f6
renzoDC15-JN <rdcarianga@joy-nostalg.com>
2024-08-01 10:24:25 +0800

    change file filter to xlsx

--- app/Filament/Resources/ContactResource.php
+++ app/Filament/Resources/ContactResource.php
@@ -244,7 +244,7 @@ class ContactResource extends Resource
                     ->form([
                         Forms\Components\FileUpload::make('file')
                             ->label('OS Report')
-                            ->acceptedFileTypes(['application/vnd.openxmlformats-officedocument.wordprocessingml.document'])
+                            ->acceptedFileTypes(['application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'])
                             ->maxSize(1024*12)
                             ->storeFiles(false),
                     ])
