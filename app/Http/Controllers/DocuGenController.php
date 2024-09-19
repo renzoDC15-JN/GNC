b0d6eada5fa8b45350c14b6495436dd31af8f2c0
renzoDC15-JN <rdcarianga@joy-nostalg.com>
2024-09-19 16:37:54 +0800

    N/A blank values

--- app/Http/Controllers/DocuGenController.php
+++ app/Http/Controllers/DocuGenController.php
@@ -93,7 +93,7 @@ class DocuGenController extends Controller
         // dd($ci);
         //set values
         foreach ($ci as $key => $value) {
-            $templateProcessor->setValue($key, htmlspecialchars($value??''));
+            $templateProcessor->setValue($key, htmlspecialchars($value==''?'N/A':$value));
         }
 
         //set image
